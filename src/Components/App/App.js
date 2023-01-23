980cd6f3990b7ede2d29de713d8a4616735ff986
MaxCLZ <maxence.clusaz@gmail.com>
2023-01-23 15:54:24 +0100

    RemoveTrack and Change playlist Name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,6 +43,8 @@ class App extends React.Component {
       ],
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -53,6 +55,17 @@ class App extends React.Component {
     }
     this.setState({ playlistTracks: tracks });
   }
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+    this.setState({
+      playlistTracks: tracks,
+    });
+  }
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -69,6 +82,8 @@ class App extends React.Component {
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,11 +5,14 @@ class Track extends React.Component {
   constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   renderAction() {
     return this.props.isRemoval ? (
-      <button className="Track-action">-</button>
+      <button className="Track-action" onClick={this.removeTrack}>
+        -
+      </button>
     ) : (
       <button className="Track-action" onClick={this.addTrack}>
         +
@@ -18,6 +21,9 @@ class Track extends React.Component {
   }
   addTrack() {
     this.props.onAdd(this.props.track);
+  }
+  removeTrack() {
+    this.props.onRemove(this.props.track);
   }
   render() {
     return (
