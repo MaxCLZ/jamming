deb1014293751c6a31f960c7b370af5324d55d33
MaxCLZ <maxence.clusaz@gmail.com>
2023-01-23 14:49:53 +0100

    Implemnetation of AddTrack to playlist tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,54 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: "name1", artist: "artist1", album: "album1", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        { name: "name4", artist: "artist4", album: "album4", id: 4 },
+      ],
+      playlistName: "My very first playslist",
+      playlistTracks: [
+        {
+          name: "playList-name1",
+          artist: "playList-artist1",
+          album: "playList-album1",
+          id: 5,
+        },
+        {
+          name: "playList-name2",
+          artist: "playList-artist2",
+          album: "playList-album2",
+          id: 6,
+        },
+        {
+          name: "playList-name3",
+          artist: "playList-artist3",
+          album: "playList-album3",
+          id: 7,
+        },
+        {
+          name: "playList-name4",
+          artist: "playList-artist4",
+          album: "playList-album4",
+          id: 8,
+        },
+      ],
+    };
+    this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    } else {
+      tracks.push(track);
+    }
+    this.setState({ playlistTracks: tracks });
+  }
   render() {
     return (
       <div>
@@ -14,8 +62,14 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,16 @@
 import React from "react";
+
 import "./Playlist.css";
+
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
   render() {
     return (
-      <div class="Playlist">
-        <input defaultValue={"New Playlist"} />
-        {/* <TrackList /> */}
-        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+      <div className="Playlist">
+        <input defaultValue={this.props.playlistName} />
+        <TrackList tracks={this.props.playlistTracks} />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
   }
